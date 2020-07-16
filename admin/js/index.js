b024f566bbfe1377e4a459ae9048944d985a7996
3128125949@qq.com <3128125949@qq.com>
2020-07-16 21:30:42 +0800

    this修改样式

--- admin/js/index.js
+++ admin/js/index.js
@@ -21,4 +21,14 @@ $(function () {
         //2.2手动修改跳转页面
         window.location.href = './login.html'
     })
+    //3.鼠标点击谁, 当前的样式修改样式
+    $('.level01').on('click', function () {
+        $(this).addClass("active").siblings().removeClass("active");
+        if ($(this).index() == 1) {
+            //3.1点击(slideToggle)依次展开或卷起某个元素
+            $('.level02').slideToggle();
+            //3.2切换类名 后代选择器 判断有没有这个类名 
+            $(this).find("b").toggleClass("rotate0");
+        }
+    })
 })
