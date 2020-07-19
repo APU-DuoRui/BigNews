f6e9d4b5cb7b4312f2d3a21ee5a80bab5942c732
3128125949@qq.com <3128125949@qq.com>
2020-07-19 22:27:39 +0800

    完成分页

--- admin/js/article_list.js
+++ admin/js/article_list.js
@@ -16,7 +16,14 @@ $(function () {
     $("#btnSearch").on("click", function (e) {
         //2.1阻止默认跳转事件
         e.preventDefault();
-        //2.2发送ajax请求
+        //2.2调用一下根据文章请求
+        getArticleList(1);
+    })
+    //2.3页面一加载就触发ajax请求
+    $("#btnSearch").trigger("click");
+
+    //3.封装根据文章2请求
+    function getArticleList(currentPage) {
         $.ajax({
             url: BigNew.article_query,
             type: "get",
@@ -25,7 +32,7 @@ $(function () {
                 type: $("#selCategory").val(),
                 state: $("#selStatus").val(),
                 //页码
-                page: 1,
+                page: currentPage,
                 //请求的次数
                 perpage: 10,
             },
@@ -33,16 +40,45 @@ $(function () {
                 console.log(backData);
                 //2.3渲染模板上
                 $("table>tbody").html(template("filtrate_list", backData));
+                //2.4调用一下根据分类查询
+                loadPageination(backData.data.totalPage, currentPage)
             }
         })
-    })
-    //2.3页面一加载就发送ajax请求
-    $("#btnSearch").click();
+    }
 
-    //3.分页
+    /**4.分页查询
+     * @description :  加载分页组件
+     * @param {type}:  totalPage : 总页码 
+     * @param {type}:  startPage : 当前页数 
+     * @return: 
+     */
+    //4.1封装一个函数 (分类查询)
+    function loadPageination(totalPage, startPage) {
+        //4.1(1)先销毁上一次分页数据
+        $("#pagination").twbsPagination("destroy")
+        //4.1(2)加载分页插件
+        $("#pagination").twbsPagination({
+            //4.1(3)总页数
+            totalPages: totalPage,
+            //4.1(4)用户可以看见的页码
+            startPage: startPage,
+            //4.1(5)
+            visiblePages: 7,
+            first: "上一页",
+            prev: "上一页",
+            next: "下一页",
+            last: "尾页",
+            onPageClick: function (event, page) {
+                //4.1(6)如果点击的页数与当前页数不一致则发送ajax请求
+                if (page != startPage) {
+                    //4.1(7)调用一下(页数请求文章列表)
+                    getArticleList(page);
+                }
+            }
+        })
+    }
 
-
-    //4.点击删除 因为这里是动态生成需要委托注册 点击
+    //5.点击删除 因为这里是动态生成需要委托注册 点击
     $("table>tbody").on("click", function (e) {
         //4.1阻止默认事件
         e.preventDefault();
@@ -96,6 +132,4 @@ $(function () {
             }
         })
     })
-
-
 })
